14ee0e5ed4b9ea6cd455adaa99e7c3b8a6e42754
CSharpProjector <marleymulvinbroome@gmail.com>
2023-03-16 21:34:26 +1100

    Implemented a basic testDriver with the functionality to open and close an app. Also implemented a basic test to see that it runs!

--- src/main.js
+++ src/main.js
@@ -1,18 +1,46 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-	});
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
 
-	win.loadFile('index.html');
+  win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
-	createWindow();
+  createWindow();
 });
 
-app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
+
+const METHODS = {
+  isReady() {
+    // do any setup needed
+    return true;
+  },
+  // define your RPC-able methods here
+};
+
+const onMessage = async ({ msgId, cmd, args }) => {
+  let method = METHODS[cmd];
+  if (!method) method = () => new Error("Invalid method: " + cmd);
+  try {
+    const resolve = await method(...args);
+    process.send({ msgId, resolve });
+  } catch (err) {
+    const reject = {
+      message: err.message,
+      stack: err.stack,
+      name: err.name,
+    };
+    process.send({ msgId, reject });
+  }
+};
+
+if (process.env.APP_TEST_DRIVER) {
+  process.on("message", onMessage);
+}
