dc37a1c24aca7917483fd188302729fe88a8236b
CSharpProjector <marleymulvinbroome@gmail.com>
2023-03-20 21:13:40 +1100

    Implemented a basic build config for electron forge

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 
+if (require('electron-squirrel-startup')) app.quit();
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 650,
